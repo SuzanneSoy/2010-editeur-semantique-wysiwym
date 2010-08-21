2f4d55a9e3f8a42be6fbb0a94001a71b035850c3
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-08-21 16:25:32 +0200

    Curseur clignotant

--- editeur.js
+++ editeur.js
@@ -1,5 +1,21 @@
 $(function() {
 	var éditeurs = $(".editeur-semantique");
 	éditeurs.append('<span class="text">hello <span class="curseur"></span>world</span>');
-	$(".curseur").show();
+	curseurClignotant();
 });
+
+function curseurClignotant() {
+	var affiché = false;
+	var clignote = function() {
+		if (affiché) {
+			$(".curseur").removeClass("affiché");
+			var délai = 500;
+		} else {
+			$(".curseur").addClass("affiché");
+			var délai = 800;
+		}
+		affiché = !affiché;
+		window.setTimeout(clignote, délai);
+	};
+	clignote();
+}
