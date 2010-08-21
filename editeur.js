fc49d77e263b43d17dab850b2e2430b39a2716a1
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-08-21 17:08:27 +0200

    Déplacement gauche-droite du curseur.

--- editeur.js
+++ editeur.js
@@ -1,7 +1,9 @@
 $(function() {
 	var éditeurs = $(".editeur-semantique");
-	éditeurs.append('<span class="text">hello <span class="curseur"></span>world</span>');
+	éditeurs.append('<span class="text">hello </span><span class="curseur"></span><span class="text">world</span>');
 	curseurClignotant();
+	raccourcisClavier();
+	curseurGauche();
 });
 
 function curseurClignotant() {
@@ -18,4 +20,46 @@ function curseurClignotant() {
 		window.setTimeout(clignote, délai);
 	};
 	clignote();
+}
+
+function curseurGauche() {
+	console.log("gauche");
+	var curseur = $(".curseur");
+	var p = curseur.prev();
+	var n = curseur.next();
+	var c = p.text().charAt(p.text().length - 1);
+	n.text(c + n.text());
+	p.text(p.text().substring(0, p.text().length - 1));
+}
+
+function curseurHaut() {
+	console.log("haut");
+}
+
+function curseurDroite() {
+	console.log("droite");
+	var curseur = $(".curseur");
+	var p = curseur.prev();
+	var n = curseur.next();
+	var c = n.text().charAt(0);
+	n.text(n.text().substring(1));
+	p.text(p.text() + c);
+}
+
+function curseurBas() {
+	console.log("bas");
+}
+
+function raccourcisClavier() {
+	var raccourcis = {
+		37 : curseurGauche,
+		38 : curseurHaut,
+		39 : curseurDroite,
+		40 : curseurBas,
+	};
+	$(document).keydown(function (e) {
+		if (raccourcis[e.keyCode]) {
+			raccourcis[e.keyCode]();
+		}
+	});
 }
